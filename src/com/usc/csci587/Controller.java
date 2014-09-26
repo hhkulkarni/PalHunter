2e3ecb00c5091595fe3d54147ed65956b38e76ec
impurva <purva.tadphale@gmail.com>
2014-09-26 00:25:28 -0700

    fixing Controller & SelectFromDb

--- src/com/usc/csci587/Controller.java
+++ src/com/usc/csci587/Controller.java
@@ -2,47 +2,6 @@ package com.usc.csci587;
 
 import java.sql.Connection;
 import java.sql.Statement;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-@Path("/controller")
-public class Controller {
-	
-	@GET
-	@Path("/setcurrentlocation")
-	@Produces(MediaType.TEXT_HTML)
-	public Response setCurrentLocation(
-    		@Context HttpHeaders headers,
-    		@QueryParam("long") String longitude, @QueryParam("lat") String lat, @QueryParam("uid") String uid)
-	{
-		UpdateDb update =new UpdateDb();
-		update.updateuserTable(longitude,lat,uid);
-		/*try {
-			Connection conn=DbUtils.getInstance("jdbc:oracle:thin:@//128.125.163.168:1521/csci585","team1","hp240312");
-			System.out.println("Input parameters longitude:"+longitude+ ",latitude :"+lat);
-			Statement stmt=conn.createStatement();
-			stmt.executeQuery("update users u SET u.user_timestamp=CURRENT_TIMESTAMP, u.location=SDO_GEOMETRY(2001, 8307, SDO_POINT_TYPE(76.3434, 98.2323, NULL), NULL, NULL) WHERE u.user_id=2");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		return Response.status(200).entity("Input parameters longitude:"+longitude+ ",latitude :"+lat).build();
-		
-	}
-
-}
-=======
-package com.usc.csci587;
-
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.ws.rs.GET;
@@ -105,4 +64,4 @@ public class Controller {
 	}
 
 }
->>>>>>> dfb4aa179f19f1184241375577c028c6f90b1061
+
--- src/com/usc/csci587/SelectFromDb.java
+++ src/com/usc/csci587/SelectFromDb.java
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-package com.usc.csci587;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import oracle.spatial.geometry.JGeometry;
-import oracle.spatial.geometry.JGeometry.Point;
-import oracle.sql.STRUCT;
-
-public class SelectFromDb {
-	
-	private Connection conn;
-	
-	
-	public SelectFromDb() {
-		super();
-		try {
-			conn=DbUtils.getInstance("jdbc:oracle:thin:@//128.125.163.168:1521/csci585","team1","hp240312");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-
-	public ArrayList<JGeometry.Point> getTrajectory(String uid){
-		ArrayList<JGeometry.Point> latlong=new ArrayList<JGeometry.Point>();
-		try {
-			
-			Statement stmt=conn.createStatement();
-			ResultSet rs=stmt.executeQuery("select t.location from trajectories t where t.user=" + uid);
-
-			STRUCT st=(STRUCT) rs.getObject("location");
-			JGeometry geom=JGeometry.load(st);
-			double[] temp=geom.getOrdinatesArray();
-			for(int i=0;i<temp.length;i+=2){
-				latlong.add(new Point(temp[i], temp[i+1])); // gtrh
-			}
-		   } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();	
-		}
-		finally{
-			org.apache.commons.dbutils.DbUtils.closeQuietly(conn);
-		}
-		return latlong;
-	}
-}
-=======
 package com.usc.csci587;
 
 import java.sql.Connection;
@@ -106,4 +52,4 @@ public class SelectFromDb {
 		return latlong;
 	}
 }
->>>>>>> dfb4aa179f19f1184241375577c028c6f90b1061
+
